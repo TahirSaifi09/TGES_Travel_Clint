0b033a348520b048fdae6a5725b1f10e91d33771
Tahir Saifi <tahirsaifi09>
2025-01-22 17:51:58 +0530

    Tour package cancilation part devide into component

--- src/Pages/TourPackage/index.js
+++ src/Pages/TourPackage/index.js
@@ -2,12 +2,14 @@ import Nav from "../../Component/home_component/navbar";
 import SideBar from "../../Component/TourPackage/sidebar";
 import Page1 from "../../Component/TourPackage/page1";
 import Footer from "../../Component/home_component/footer";
+import MoreDetails from "../../Component/TourPackage/cancelation";
 
 export default function TourPackage(){
     return(
         <div>
             <Nav />
             <Page1 />
+            <MoreDetails />
             <SideBar />
             <Footer />
         </div>
